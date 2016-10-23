9736049932f79bd29425e6cb1e22e153063a383f
Ken <kenkjw@gmail.com>
2016-10-22 22:03:07 -0700

    Improve Movie class docstring

--- media.py
+++ media.py
@@ -1,5 +1,12 @@
 class Movie():
-    """ This class provides a way to store movie related information"""
+    """ This class provides a way to store movie related information
+
+    Attributes:
+        title: A string representing the title of the movie.
+        storyline: A short description of the movie.
+        poster_image_url: A url to an image file of the poster of the movie
+        trailer_youtube_url: A url to a youtube video of the trailer of the movie.
+    """
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
